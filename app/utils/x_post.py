0ffa600e23883b72b784d26704c8bc5eee07727b
qoder1337 <149431788+qoder1337@users.noreply.github.com>
2024-12-03 23:43:28 +0100

    auth fertig

--- app/utils/x_post.py
+++ app/utils/x_post.py
@@ -1,12 +1,13 @@
-from requests_oauthlib import OAuth1Session
 import json, os
+from requests_oauthlib import OAuth1Session
+from app.config import BASEDIR
 from dotenv import load_dotenv
 load_dotenv()
 
 xuser_dict = {
     "bestinpoker": {
         "consumer_key" : os.getenv("XBIP_CONSUMER_KEY"),
-        "consumer_secret" : os.getenv("XBIP_CONSUMER_SECRET")
+        "consumer_secret" : os.getenv("XBIP_CONSUMER_SECRET"),
     }
 }
 
@@ -33,7 +34,51 @@ class BaseforX():
 class AuthorizeOnX(BaseforX):
     def __init__(self, xuser):
         super().__init__(xuser)
-        pass
+
+    def authrequest(self, xuser):
+        request_token_url = "https://api.twitter.com/oauth/request_token?oauth_callback=oob&x_auth_access_type=write"
+        oauth = OAuth1Session(self.consumer_key, client_secret=self.consumer_secret)
+
+        try:
+            fetch_response = oauth.fetch_request_token(request_token_url)
+        except ValueError as e:
+            print("Error fetching request token:", str(e))
+            return
+
+        resource_owner_key = fetch_response.get("oauth_token")
+        resource_owner_secret = fetch_response.get("oauth_token_secret")
+        print("Got OAuth token: %s" % resource_owner_key)
+
+        # Get authorization
+        base_authorization_url = "https://api.twitter.com/oauth/authorize"
+        authorization_url = oauth.authorization_url(base_authorization_url)
+        print("Please go here and authorize: %s" % authorization_url)
+        verifier = input("Paste the PIN here: ")
+
+
+        # access token holen
+        access_token_url = "https://api.twitter.com/oauth/access_token"
+
+        oauth = self.create_session(resource_owner_key, resource_owner_secret, verifier)
+        oauth_tokens = oauth.fetch_access_token(access_token_url)
+
+        access_token = oauth_tokens["oauth_token"]
+        access_token_secret = oauth_tokens["oauth_token_secret"]
+
+        tokens = {
+            "access_token": access_token,
+            "access_token_secret": access_token_secret,
+        }
+
+        # Speichern des Access-Tokens
+        token_dir = os.path.join(BASEDIR, "tokens")
+        token_filepath = os.path.join(token_dir, f"x-{xuser}.json")
+        with open(token_filepath, "w") as token_file:
+            json.dump(tokens, token_file)
+        print(f"Tokens saved to {token_filepath}")
+
+
+
 
 class PostOnX(BaseforX):
     def __init__(self, xuser):
