584770caa0eef9e29054a03f92f801182f03512a
qoder1337 <149431788+qoder1337@users.noreply.github.com>
2025-01-09 20:38:00 +0100

    123

--- run.py
+++ run.py
@@ -7,25 +7,20 @@ from app.main import random_latest_tweet, clean_tweet_dbs
 
 app = create_app("production")
 
-with app.app_context():
-    ### TEST
-    # try:
-    #     random_latest_tweet()
-    # except Exception as e:
-    #     print(e)
+# with app.app_context():
+#     ### TEST
+#     try:
+#         random_latest_tweet()
+#     except Exception as e:
+#         print(e)
 
-    schedule.every(random.randint(85, 90)).minutes.do(random_latest_tweet)
-    schedule.every().monday.do(clean_tweet_dbs)
+#     # schedule.every(random.randint(85, 90)).minutes.do(random_latest_tweet)
+#     # schedule.every().monday.do(clean_tweet_dbs)
 
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+#     while True:
+#         schedule.run_pending()
+#         time.sleep(1)
 
 
-# if __name__ == "__main__":
-
-#     app.run(
-#         debug=True,
-#         use_reloader=False,
-#         port=4444
-#     )
+if __name__ == "__main__":
+    app.run(debug=True, use_reloader=False, port=4444)
